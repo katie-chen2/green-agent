6a44d25208b2f5a9547662c15b927be031486fa6
katie-chen2 <cweiyuan9@gmail.com>
2026-01-18 18:21:28 -0500

    code green agent

--- src/agent.py
+++ src/agent.py
@@ -1,3 +1,7 @@
+from wsgiref import types
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types as genai_types
 from a2a.server.tasks import TaskUpdater
 from a2a.types import Message, TaskState, Part, TextPart
 from a2a.utils import get_message_text, new_agent_text_message
@@ -7,8 +11,14 @@ from messenger import Messenger
 
 class Agent:
     def __init__(self):
-        self.messenger = Messenger()
+        # self.messenger = Messenger()
         # Initialize other state here
+        load_dotenv()
+        self.messages = []
+        self.client = genai.Client()
+        self.config = genai_types.GenerateContentConfig(
+            system_instruction="You are an evaluator. Your task is to provide accurate and reliable evaluations for agentic responses to benchmark queries.",
+        )
 
     async def run(self, message: Message, updater: TaskUpdater) -> None:
         """Implement your agent logic here.
@@ -20,13 +30,19 @@ class Agent:
         Use self.messenger.talk_to_agent(message, url) to call other agents.
         """
         input_text = get_message_text(message)
+        print(f"> Received message: {input_text}")
+        self.messages.append(input_text)
 
         # Replace this example code with your agent logic
 
         await updater.update_status(
             TaskState.working, new_agent_text_message("Thinking...")
         )
+        response = self.client.models.generate_content(
+            model="gemini-3-flash-preview", contents=self.messages, config=self.config
+        )
+        print(response.text)
         await updater.add_artifact(
-            parts=[Part(root=TextPart(text=input_text))],
-            name="Echo",
+            parts=[Part(root=TextPart(text=response.text if response.text else "No response"))],
+            name="Response from Gemini-3",
         )
--- src/server.py
+++ src/server.py
@@ -24,16 +24,16 @@ def main():
     # See: https://a2a-protocol.org/latest/tutorials/python/3-agent-skills-and-card/
     
     skill = AgentSkill(
-        id="",
-        name="",
-        description="",
-        tags=[],
-        examples=[]
+        id="chat",
+        name="Chat",
+        description="Answer questions and have conversations.",
+        tags=['chat', 'conversation', 'questions'],
+        examples=['hi, tell me about AI.']
     )
 
     agent_card = AgentCard(
-        name="",
-        description="",
+        name="Simple Agent",
+        description="A Conversational Agent that uses Gemini-3 to answer questions.",
         url=args.card_url or f"http://{args.host}:{args.port}/",
         version='1.0.0',
         default_input_modes=['text'],
